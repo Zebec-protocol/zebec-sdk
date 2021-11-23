ed1a0994cfb67fabf589f039e1a3c418a8e028e3
raghibislam <islamraghib3@gmail.com>
2021-11-24 00:32:57 +0545

    SDK refactored added new programID

--- constants.js
+++ constants.js
@@ -1,22 +1,23 @@
-const {
-    PublicKey,
-    Connection,
-    clusterApiUrl
-  } = require("@solana/web3.js");
-  
+const { PublicKey, Connection, clusterApiUrl } = require("@solana/web3.js");
 
-const base58publicKey = new PublicKey("9Ayh2hS3k5fTn6V9Ks7NishUp5Jz19iosK3tYPAcNhsp");
-const PROGRAM_ID = "9Ayh2hS3k5fTn6V9Ks7NishUp5Jz19iosK3tYPAcNhsp"; // Zebec program id
-const CLUSTER = "devnet"
+const base58publicKey = new PublicKey(
+  "7FNWTfCo3AyRBFCvr49daqKHehdn2GjNgpjuTsqy5twk"
+);
+const PROGRAM_ID = "7FNWTfCo3AyRBFCvr49daqKHehdn2GjNgpjuTsqy5twk"; // Zebec program id
+const CLUSTER = "devnet";
 const connection = new Connection(clusterApiUrl(CLUSTER)); // cluster
-const SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID = new PublicKey("ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL");
-const wallettokenaddress = new PublicKey("2ibSirDWk5P68ZKmQQSxUMtiWQFRuanpPfMfaYzxgSRv");
+const SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID = new PublicKey(
+  "ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL"
+);
+const wallettokenaddress = new PublicKey(
+  "2ibSirDWk5P68ZKmQQSxUMtiWQFRuanpPfMfaYzxgSRv"
+);
 
 module.exports.constants = {
-    base58publicKey,
-    PROGRAM_ID,
-    CLUSTER,
-    connection,
-    SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID,
-    wallettokenaddress
-}
+  base58publicKey,
+  PROGRAM_ID,
+  CLUSTER,
+  connection,
+  SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID,
+  wallettokenaddress,
+};
