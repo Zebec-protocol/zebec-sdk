c94fb304745240506dda77b3b8a3beb8376811a9
raghibislam <islamraghib3@gmail.com>
2022-02-03 19:23:46 +0545

    SPL TOken added

--- constants.js
+++ constants.js
@@ -3,16 +3,19 @@ const base58publicKey = new PublicKey(
   "AknC341xog56SrnoK6j3mUvaD1Y7tYayx1sxUGpeYWdX"
 );
 const PROGRAM_ID = "AknC341xog56SrnoK6j3mUvaD1Y7tYayx1sxUGpeYWdX"; // Zebec program id
- const CLUSTER = "devnet";
- const connection = new Connection(clusterApiUrl(CLUSTER)); // cluster
+const CLUSTER = "devnet";
+const connection = new Connection(clusterApiUrl(CLUSTER)); // cluster
 const SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID = new PublicKey(
   "ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL"
 );
 const wallettokenaddress = new PublicKey(
   "2ibSirDWk5P68ZKmQQSxUMtiWQFRuanpPfMfaYzxgSRv"
 );
+const ATOKEN = "ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL";
 const stringofwithdraw = "withdraw_sol";
-const FEEADDRESS = "EsDV3m3xUZ7g8QKa1kFdbZT18nNz8ddGJRcTK84WDQ7k"
+const FEEADDRESS = "EsDV3m3xUZ7g8QKa1kFdbZT18nNz8ddGJRcTK84WDQ7k";
+const TOKENPROGRAMID = "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA";
+const SYSTEMRENT = "SysvarRent111111111111111111111111111111111";
 module.exports.constants = {
   base58publicKey,
   PROGRAM_ID,
@@ -21,5 +24,8 @@ module.exports.constants = {
   SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID,
   wallettokenaddress,
   stringofwithdraw,
-  FEEADDRESS
+  FEEADDRESS,
+  ATOKEN,
+  TOKENPROGRAMID,
+  SYSTEMRENT,
 };
